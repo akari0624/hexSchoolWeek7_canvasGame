b3473d8f8c94bcf6c49b45e4950be14a236c1e69
akari0624 <akari0624@hotmail.com>
2018-07-23 19:10:50 +0800

    fix wrong bullet update method

--- src/battleShipModel/Bullet.js
+++ src/battleShipModel/Bullet.js
@@ -1,27 +1,17 @@
 
-const baseX = 45
+const baseX = 60
 
-const baseY = 45
+const baseY = 60
 
 const BulletFactory = (type, angle) => {
   
   //從砲口射出去,所以不會是0,如果是0 就是從中心射出去
-  // 根據轉動當下的角度不同 所在不同象限 起始座標就不同 
+  const angle2 = angle * (Math.PI / 180) 
+  const iX = Math.cos(angle2) * baseX
+  const iY = Math.sin(angle2) * baseY
 
-  if(angle >= 0 && angle <= 90){
-    return new Bullet(type, angle, baseX, baseY)
-  }
-  else if(angle >= 91 && angle <= 180){
-    return new Bullet(type, angle, -baseX, baseY)
-  } 
-  else if(angle <= -91 && angle >= -180){
-    return new Bullet(type, angle, -baseX, -baseY)  
-  }
-
-  // angle <= -1 && angle <= -90
-  else{ 
-    return new Bullet(type, angle, baseX, -baseY)   
-  }
+  return new Bullet(type, angle2, iX, iY)   
+  
 }
 
 
@@ -38,7 +28,6 @@ class Bullet{
   draw(ctx, centerX, centerY,BULLET_RADIUS){
 
     ctx.translate(centerX, centerY)
-    // ctx.rotate((this.angle - 45) * Math.PI / 180)
     ctx.beginPath()
     ctx.arc(this.x, this.y, BULLET_RADIUS, 0, 2 * Math.PI, false)
     ctx.fill()
@@ -51,24 +40,12 @@ class Bullet{
 
   update(bulletSpeed){
     const angle = this.angle
-    if(angle >= 0 && angle <= 90){
-      this.x = this.x + bulletSpeed
-      this.y = this.y + bulletSpeed
-    }
-    else if(angle >= 91 && angle <= 180){
-      this.x = this.x - bulletSpeed
-      this.y = this.y + bulletSpeed
-    }
-    else if(angle <= -91 && angle >= -180){
-      this.x = this.x - bulletSpeed
-      this.y = this.y - bulletSpeed
-    }
 
-    //  angle <= -1 && angle <= -90
-    else{
-      this.x = this.x + bulletSpeed
-      this.y = this.y - bulletSpeed
-    }
+    const iX = Math.cos(angle) * bulletSpeed
+    const iY = Math.sin(angle) * bulletSpeed
+
+    this.x +=  iX
+    this.y +=  iY
 
   }
 
